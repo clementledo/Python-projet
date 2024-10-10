b73cf9b95fda083c2096bdb26f0107f5cbf0529a
RENL36 <rentylucie@gmail.com>
2024-10-10 19:32:38 +0200

    Update carte.py

--- models/carte.py
+++ models/carte.py
@@ -1,6 +1,6 @@
 import random
 
-"""classe Tile pas utile je pense (pour la vue terminale)"""
+
 class Tile:
     """Classe représentant une seule tuile."""
     def __init__(self, tile_type):
@@ -19,14 +19,10 @@ class Carte:
             for y in range(self.hauteur):
                 # Choisir un type de tuile aléatoire, par exemple : 0 = plaine, 1 = forêt, 2 = montagne
                 if type_carte == "ressources_generales":
-                    """tile = random.choice(['W','T','G', 'F','v'])"""
                     tile_type = random.choice([0, 1, 2])  # Plain, forêt, montagne
                 elif type_carte == "or_central":
                     if 40 < x < 80 and 40 < y < 80:  # Mettons que l'or est concentré au centre
-                        """tile='G'"""
                         tile_type = 3  # Type spécial pour l'or
                     else:
-                        """tile = random.choice(['W','T','G', 'F','v'])"""
                         tile_type = random.choice([0, 1, 2])
-                """self.grille[x][y] = tile"""
                 self.grille[x][y] = Tile(tile_type)
