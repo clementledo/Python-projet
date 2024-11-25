140f7ec7f96bf439641556358d7851cb89cca3dd
byn227 <vinh_tri.nguyen@insa-cvl.fr>
2024-11-25 15:54:51 +0100

    Update and rename Type.py to Resource_Type.py

---
+++ models/Resources/Resource_Type.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class type(Enum):
+    """
+    Types of ressources :
+        Wood
+        Food
+        Gold
+     
+    """
+
+    Wood = 1
+    Food = 2
+    Gold = 3
+
