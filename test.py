5e7a4e7fcf56866e706a33e71346c51bfe168dd0
tilevadoux <tim.levadoux@etu.uca.fr>
2025-01-07 14:11:52 +0100

    first push

--- test.py
+++
@@ -1,8 +0,0 @@
-from models.map import Map
-
-#def main():
-
-carte = Map(120, 120)
-carte.generer_aleatoire(type_carte="centre_ressources")
-for y in range(carte.hauteur):
-    print(" ".join([tile.resource[0] if tile.resource else " " for tile in carte.grille[y]]))
